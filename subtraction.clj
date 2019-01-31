63eb7e6cd359d90ef9e6328d33792106f82ee02b
kattakavya <kavya@front.live>
2019-01-31 19:51:38 +0530

    Branch testing

---
+++ subtraction.clj
@@ -0,0 +1,12 @@
+(ns desktop-page.subtraction
+    (:use
+         [clojure.test :refer :all]
+         [desktop-page.core :refer :all]))
+
+(defn hello []
+  (println "welcome to testing "))
+(hello)
+
+(is (= 5(+ 2 2)
+
+
